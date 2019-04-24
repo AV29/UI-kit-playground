6c62ca54d46f9a40d8914b10fc522c58d6403361
AntonVlasik <antonvlasik@coherentsolutions.com>
2019-04-24 17:58:33 +0300

    removed unnecessary object from Switch

--- src/components/ToolBox/controls/Switch/Switch.jsx
+++ src/components/ToolBox/controls/Switch/Switch.jsx
@@ -2,31 +2,6 @@ import React from 'react';
 import Switch from 'react-toolbox/lib/switch';
 import theme from './Switch.scss';
 
-const styles = {
-  switchBase: {
-    color: '#ffffff',
-    '&$checked': {
-      color: '#004a1c',
-      '& + $bar': {
-        opacity: 1,
-        backgroundColor: '#009639',
-      },
-      '&$disabled + $bar': {
-        backgroundColor: '#ececec',
-      }
-    },
-    '& + $bar': {
-      color: '#bfbfbf'
-    },
-    '&$disabled': {
-      color: '#e6e6e6'
-    }
-  },
-  bar: {},
-  checked: {},
-  disabled: {}
-};
-
 export default (({ ...props }) => {
   return (
     <Switch
